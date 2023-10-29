b68aad25923e99438a34f139492a3cf221a4381c
Ofer Sadan <ofersadan85@gmail.com>
2023-10-29 20:55:21 +0200

    Update trivia.py with random questions

--- projects/trivia.py
+++ projects/trivia.py
@@ -1,4 +1,5 @@
-# A trivia game
+import json
+import random
 
 
 def display_score(score):
@@ -11,7 +12,7 @@ def check_answer(possible_answers, score):
     user_answer = input("Your answer: ")
     exit_words = ["exit", "quit", "q", "x"]
     if user_answer.lower() in exit_words:
-           exit()
+        exit()
     if user_answer.lower() in possible_answers:
         score = score + 1
         print("Correct!")
@@ -19,27 +20,30 @@ def check_answer(possible_answers, score):
         print("Incorrect!")
     return score
 
-def ask_question(question_text, answer, current_score):
-    print(question_text)
-    score = check_answer(answer, current_score)
+
+def ask_question(question_object, current_score):
+    print(question_object["text"])
+    if "wrong" in question_object.keys():
+        letters = "ABCDEFG"
+        options = question_object["wrong"] + [question_object["answer"][0].capitalize()]
+        random.shuffle(options)
+        for option in zip(options, letters):
+            if option[0].lower() in question_object["answer"]:
+                question_object["answer"].append(option[1].lower())
+            option = "{}. {}".format(option[1], option[0])
+            print(option)
+    score = check_answer(question_object["answer"], current_score)
     display_score(score)
     return score
 
-question1 = "Question 1: What is the capital of France?"
-answer1 = ["paris"]
-question2 = """Question 2: What is the capital of Germany?
-A. Berlin
-B. Munich
-C. Hamburg
-D. Frankfurt"""
-answer2 = ["a", "berlin"]
 
-number_of_tries = 0
-while number_of_tries < 3:
-    print("STARTING A NEW GAME")
-    score = ask_question(question1, answer1, 0)
-    score = ask_question(question2, answer2, score)
-    display_score(score)
-    number_of_tries = number_of_tries + 1
+questions_file = open("questions.json", "r")
+questions = json.load(questions_file)
+questions_file.close()
+
+score = 0
+random.shuffle(questions)
+for question in questions:
+    score = ask_question(question, score)
 
 print("Game over, thanks for playing!")
