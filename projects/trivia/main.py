035265410cf18a297cf00703c9cf60e8836c2566
Ofer Sadan <ofersadan85@gmail.com>
2023-11-12 21:16:15 +0200

    Questions from json

--- projects/trivia/main.py
+++ projects/trivia/main.py
@@ -1,34 +1,40 @@
+import json
 import random
 
 
 def display_score(score):
-    print(f"Your score is now: {score}") # print("Your score is now:", score)
-    if score == 0:
-        print("This is not great!")
+    print(f"Your score is now: {score}. {'This is not great!' if score == 0 else ''}")
 
 
 class Question:
-    def __init__(self, text, answer, wrong):
+    def __init__(self, text, answer, wrong=False):
         self.text = text
         self.answer = answer
         self.wrong = wrong
 
+    def __str__(self):
+        return self.text
+
     def ask(self, current_score):
-        print(self.text)
-        letters = "ABCDEFG"  # This is a string, so we can iterate over it to get A, B, C, etc.
-        options = self.wrong + [self.answer[0]]  # Add the correct answer to the list of wrong answers
-        random.shuffle(options)  # Shuffle the options so the correct answer isn't always the last one
-        for item in zip(letters, options):  # zip() lets us iterate over two lists at the same time, an `item` will be a tuple of (letter, answer)
-            letter = item[0]  # This is the letter, like "A"
-            option = item[1]  # This is one of the possible options, like "Paris"
-            if (option.lower() in self.answer):  # If this is the correct answer...
-                self.answer.append(letter.lower())  # ...add the letter to the list of possible answers
-            option = "{}. {}".format(letter, option.capitalize())  # This will format the option to be "A. Option"
-            print(option)
+        print(self)
+        letters = "ABCDEFG"
+        if self.wrong:
+            options = self.wrong + [self.answer[0]]
+            random.shuffle(options)
+            for item in zip(letters, options):
+                # zip() - iterate over two lists at the same time
+                # an `item` will be a tuple of (letter, answer)
+                letter = item[0]  # This is the letter, like "A"
+                option = item[1]  # This is one of the possible options, like "Paris"
+                if option.lower() in self.answer:
+                    # If this is the correct answer add the letter to the list answers
+                    self.answer.append(letter.lower())
+                option = f"{letter}. {option.capitalize()}"
+                print(option)
         score = self.check_answer(current_score)
         display_score(score)
         return score
-    
+
     def check_answer(self, score):
         user_answer = input("Your answer: ")
         exit_words = ["exit", "quit", "q", "x"]
@@ -42,12 +48,23 @@ class Question:
         return score
 
 
-question1 = Question(
-    text="What is the capital city of Germany?",
-    answer=["berlin"],
-    wrong=["Munich", "Hamburg"]
-)
+question_file = open("questions.json", "r")
+questions = json.load(question_file)
+question_file.close()
+
+
+# questions_objects = [Question(q["text"], q["answer"], q.get("wrong")) for q in questions]
+# questions_objects = [Question(**q) for q in questions]
+questions_objects = []
+for q in questions:
+    # q = Question(text=q["text"], answer=q["answer"], wrong=q["wrong"])
+    q = Question(**q)
+    questions_objects.append(q)
+
+random.shuffle(questions_objects)
 
 score = 0
-score = question1.ask(score)
+for q in questions_objects:
+    score = q.ask(score)
+
 print("Game over, thanks for playing!")
