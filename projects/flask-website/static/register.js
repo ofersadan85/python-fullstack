69744c7c56e1c0c447a88ebd3b30cd89303de193
Ofer Sadan <ofersadan85@gmail.com>
2024-01-08 08:09:49 +0200

    match register js to classroom code

--- projects/flask-website/static/register.js
+++ projects/flask-website/static/register.js
@@ -1,79 +1,62 @@
 "use strict";
 
+let password = document.getElementById("password");
+let password2 = document.getElementById("password2");
+let password_error = document.getElementById("password_error");
+let username = document.getElementById("username-box");
+let username_error = document.getElementById("username_error");
 let registerButton = document.querySelector("#register input[type=submit]");
 
 function checkPasswordsMatch() {
-  let password = document.getElementById("password");
-  let password2 = document.getElementById("password2");
-  let error = document.getElementById("password_error");
   if (password.value !== password2.value) {
     password2.style.backgroundColor = "red";
-    error.innerHTML = "Passwords do not match";
+    password_error.innerHTML = "Passwords do not match";
     registerButton.disabled = true;
   } else {
     password2.removeAttribute("style");
-    error.innerHTML = "";
-    registerButton.disabled = false;
-  }
-}
-
-function checkUsername() {
-  let username = document.getElementById("username");
-  let error = document.getElementById("username_error");
-  if (username.value.length < 4) {
-    username.style.backgroundColor = "red";
-    error.innerHTML = "Username must be at least 4 characters";
-    registerButton.disabled = true;
-  } else {
-    username.removeAttribute("style");
-    error.innerHTML = "";
+    password_error.innerHTML = "";
     registerButton.disabled = false;
   }
 }
 
 async function checkUsernameExists() {
-  let username = document.getElementById("username");
-  let error = document.getElementById("username_error");
   fetch("/auth/user_exists?username=" + username.value)
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
       if (data.user_exists) {
         username.style.backgroundColor = "red";
-        error.innerHTML = "Username already exists";
+        username_error.innerHTML = "Username already exists";
         registerButton.disabled = true;
       } else {
         username.removeAttribute("style");
-        error.innerHTML = "";
+        username_error.innerHTML = "";
         registerButton.disabled = false;
       }
     });
 }
 
 async function checkUsernameExistsAsync() {
-  let username = document.getElementById("username");
-  let error = document.getElementById("username_error");
   let response = await fetch("/auth/user_exists?username=" + username.value);
   let data = await response.json();
   console.log(data);
   if (data.user_exists) {
     username.style.backgroundColor = "red";
-    error.innerHTML = "Username already exists";
+    username_error.innerHTML = "Username already exists";
     registerButton.disabled = true;
   } else {
     username.removeAttribute("style");
-    error.innerHTML = "";
+    username_error.innerHTML = "";
     registerButton.disabled = false;
   }
 }
 
-document.getElementById("username").addEventListener("keyup", checkUsername);
+document
+  .getElementById("password2")
+  .addEventListener("input", checkPasswordsMatch);
 document
   .getElementById("username")
   .addEventListener("focusout", checkUsernameExists);
 // document
 //   .getElementById("username")
 //   .addEventListener("focusout", checkUsernameExistsAsync);
-document
-  .getElementById("password2")
-  .addEventListener("keyup", checkPasswordsMatch);
