bdff856c90be7309d4ef6969cf840db72f17792c
Ofer Sadan <ofersadan85@gmail.com>
2024-02-07 21:29:48 +0300

    start of cookie & login logout example

--- projects/sales-website/main.py
+++ projects/sales-website/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, make_response
 from views.users import bp as users_bp
 from views.products import bp as products_bp
 from views.carts import bp as carts_bp
@@ -12,7 +12,17 @@ app.register_blueprint(carts_bp)
 
 @app.route("/")
 def main_page():
-    return "HOME PAGE"
+    print(request.cookies)
+    response = make_response("HOME PAGE")
+    response.set_cookie("user_id", "7")
+    return response
+
+
+@app.route("/logout")
+def logout():
+    response = make_response("YOU LOGGED OUT")
+    response.delete_cookie("user_id")
+    return response
 
 
 app.run(debug=True)
--- projects/sales-website/views/carts.py
+++ projects/sales-website/views/carts.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from db import get_db
 import json
 
@@ -6,12 +6,11 @@ bp = Blueprint("carts", __name__)
 
 @bp.route("/add_to_cart/<product_id>")
 def add_to_cart(product_id):
-    cart_id = 6
-    user_id = 5
+    user_id = request.cookies["user_id"]
     db = get_db()
     cursor = db.cursor()
 
-    cursor.execute("SELECT products FROM carts WHERE id = ? AND user_id = ?", [cart_id, user_id])
+    cursor.execute("SELECT products FROM carts WHERE user_id = ?", [user_id])
     old_value = cursor.fetchone()  # ('5',)
     old_value_dict = json.loads(old_value[0])  # 5
 
@@ -20,12 +19,11 @@ def add_to_cart(product_id):
     else:
         old_value_dict[product_id] = 1
     new_value = json.dumps(old_value_dict)
-    params = [new_value, cart_id, user_id]
+    params = [new_value, user_id]
 
     query = """
 UPDATE carts set products = ?
-WHERE id = ?
-AND user_id = ?
+WHERE user_id = ?
 """
     cursor.execute(query, params)
     db.commit()
