eecf943250511c8dce18aad3940ccb5a14a6046d
Ofer Sadan <ofersadan85@gmail.com>
2024-02-11 20:50:07 +0300

    add to cart in JS when user not logged in

--- projects/sales-website/static/js/cart_add.js
+++ projects/sales-website/static/js/cart_add.js
@@ -1,9 +1,43 @@
-async function addToCart(event) {
-    let button = event.target;
-    let response = await fetch("/add_to_cart/" + button.id)
-    if (response.status == 200) {
-        console.log("Added to cart " + button.id);
-    } else {
-        console.log("Failed to add to cart " + button.id);
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
     }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+async function addToUserCart(productID) {
+  console.log("User is logged in with user_id:");
+  let response = await fetch("/add_to_cart/" + productID);
+  if (response.status == 200) {
+    console.log("Added to cart " + productID);
+  } else {
+    console.log("Failed to add to cart " + productID);
+  }
+}
+
+function addToCookieCart(productID) {
+  let cart_string = getCookie("cart") == "" ? "{}" : getCookie("cart");
+  let cart = JSON.parse(cart_string);
+  cart[productID] = cart[productID] === undefined ? 1 : cart[productID] + 1;
+  cart_string = JSON.stringify(cart);
+  document.cookie = "cart=" + cart_string;
+}
+
+async function addToCart(event) {
+  let button = event.target;
+  let user_id = getCookie("user_id");
+  if (user_id == "") {
+    addToCookieCart(button.id);
+  } else {
+    addToUserCart(button.id);
+  }
 }
