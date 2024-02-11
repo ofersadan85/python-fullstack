3d9aef41672f5faa8b2cb3056026c261ae5445d9
Ofer Sadan <ofersadan85@gmail.com>
2024-02-11 18:42:39 +0300

    added login/logout logic with cookies

--- projects/sales-website/main.py
+++ projects/sales-website/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, render_template, redirect
+from db import get_db
 from views.users import bp as users_bp
 from views.products import bp as products_bp
 from views.carts import bp as carts_bp
@@ -12,15 +13,29 @@ app.register_blueprint(carts_bp)
 
 @app.route("/")
 def main_page():
-    print(request.cookies)
-    response = make_response("HOME PAGE")
-    response.set_cookie("user_id", "7")
-    return response
+    if "username" in request.args:
+        username = request.args["username"]
+        db = get_db()
+        cursor = db.cursor()
+        cursor.execute("SELECT id FROM users WHERE username = ?", [username])
+        user_id = cursor.fetchone()  # (7,)  # None
+        if user_id is not None:  # if user_id:
+            user_id = user_id[0]  # 7
+            response = redirect("/products")
+            response.set_cookie("user_id", str(user_id))
+            return response
+        else:
+            return render_template("login.html", error_messages="Username does not exist")
+
+    if "user_id" in request.cookies:
+        return redirect("/products")
+    else:
+        return render_template("login.html")
 
 
 @app.route("/logout")
 def logout():
-    response = make_response("YOU LOGGED OUT")
+    response = redirect("/")
     response.delete_cookie("user_id")
     return response
 
